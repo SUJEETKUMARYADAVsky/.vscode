bbae1f714c8c3db762ebfd131f1ba4cb0adaec0d
Sujeet Kumar Yadav <sujeetiit54321@gmail.com>
2023-07-04 15:16:00 +0530

    Make sme if the changes

---
+++ bitswise.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=7;
+    int b=4;
+
+      //  cout <<"a&b"<<"=" << (a&b) << endl;
+       // cout <<"a|b" <<"="<< (a|b) << endl;
+       //   cout <<"~a" <<"="<< (~a) << endl;
+        //cout <<"a^b" <<"="<< (a^b) << endl;
+        //cout<< (++i)<< endl;
+       //cout<< (i++)<< endl;
+        //cout << (i--) <<endl;
+        //cout << (--i) <<endl;
+        
+     if (++a)
+      
+        cout << b;
+        else
+        cout
+        <<++b;/* code */
+    
+        
+
+        return 0;
+}  
---
+++ pract.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n=4;
+    cout <<(10*( ++n ));
+    return 0;
+}
