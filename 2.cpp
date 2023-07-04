9c189d096b013092b2d8e08809452ccecaf4d42e
Sujeet Kumar Yadav <sujeetiit54321@gmail.com>
2023-07-04 15:15:49 +0530

    Done some of fthe changes

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int x,y,sum;
+   cout << " Enter two number:" << endl;
+    cin >> x >>y;
+    sum=x+y;
+    cout << x << " + " << y <<"="<< sum;
+    return 0;
+
+} 
